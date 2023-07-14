8428d24ae4cebec69a4658ee6d6e3e7a16a64a86
Danish Siddiqui <127804027+ProDanish203@users.noreply.github.com>
2023-07-15 01:19:01 +0500

    Initial Project

---
+++ client/src/Config/Router.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Routes, Route } from "react-router-dom";
+
+export const Router = () => {
+  return (
+    <>
+    <Routes>
+        <Route path='/'/>
+    </Routes>
+    </>
+  )
+}
